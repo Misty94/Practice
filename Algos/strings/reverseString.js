bc5fb1765d56d3fcbdd812947f238ca67d282b55
Misty94 <mms.strickland@gmail.com>
2022-09-27 13:51:30 -0400

    strings algo

---
+++ Algos/strings/reverseString.js
@@ -0,0 +1,18 @@
+function spinWords(string){
+    console.log(string)
+    for (let i = 0; i < string.length; i++){
+        console.log(string[i]);
+    }
+    // let newStr = '';
+    // if (string.length < 5){
+    //     newStr.push(string)
+    // }
+    // if (string.length >= 5){
+    //     for (let i = string.lengt; i >= 0; i--){
+    //         newStr.push(string[i])
+    //     }
+    // }
+    // return newStr
+}
+
+spinWords("Welcome Misty");
