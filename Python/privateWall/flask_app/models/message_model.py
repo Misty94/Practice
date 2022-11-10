ecd427cf924ab0eab7e9ca9a4ac66b047b3706a3
Misty94 <mms.strickland@gmail.com>
2022-11-10 00:14:32 -0500

    Private Wall

--- Python/privateWall/flask_app/models/message_model.py
+++ Python/privateWall/flask_app/models/message_model.py
@@ -8,7 +8,9 @@ class Message:
         self.id = data['id']
         self.content = data['content']
         self.user_id = data['user_id']
+        self.user = data['user']
         self.friend_id = data['friend_id']
+        self.friend = data['friend']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
 
@@ -31,7 +33,7 @@ class Message:
         query = "SELECT users.first_name AS user, users2.first_name AS friend, messages.* "
         query += "FROM users LEFT JOIN messages ON users.id = messages.user_id "
         query += "LEFT JOIN users AS users2 ON users2.id = messages.friend_id "
-        query += "WHERE users2.id = 1;" # %(friend_id)s ??? why is this not working
+        query += "WHERE users2.id = 2;" # %(friend_id)s ??? why is this not working
 
         results = connectToMySQL( DATABASE ).query_db(query, data)
         messages = []
