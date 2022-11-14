4f25813ffd2078ecfc1a1d2dbb14fd1d11da03a1
Misty94 <mms.strickland@gmail.com>
2022-11-14 14:13:13 -0500

    Private Wall Completed

--- Python/privateWall/flask_app/models/message_model.py
+++ Python/privateWall/flask_app/models/message_model.py
@@ -2,6 +2,8 @@ from flask_app.config.mysqlconnection import connectToMySQL
 from flask import flash
 from flask_app import DATABASE
 from flask_app.models.user_model import User
+from datetime import datetime
+import math
 
 class Message:
     def __init__(self, data):
@@ -13,6 +15,20 @@ class Message:
         self.friend = data['friend']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
+
+    def time_span(self):
+        now = datetime.now()
+        delta = now - self.created_at
+        print(delta.days)
+        print(delta.total_seconds())
+        if delta.days > 0:
+            return f"{delta.days} days ago"
+        elif (math.floor(delta.total_seconds() / 60 )) >= 60:
+            return f"{math.floor(math.floor(delta.total_seconds() / 60) / 60)} hours ago"
+        elif delta.total_seconds() >= 60:
+            return f"{math.floor(delta.total_seconds() / 60)} minutes ago"
+        else:
+            return f"{math.floor(delta.total_seconds())} seconds ago"
 
     @classmethod
     def save(cls, data):
