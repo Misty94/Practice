b75842546d1009b51e19ba1f75553608e4939a88
Misty94 <mms.strickland@gmail.com>
2022-12-01 23:56:09 -0500

    messages

--- Python/teachersUnited/flask_app/controllers/posts_controller.py
+++ Python/teachersUnited/flask_app/controllers/posts_controller.py
@@ -1,7 +1,7 @@
 from flask_app import app
 from flask import render_template, request, redirect, session, flash
 from flask_app.models.post_model import Post
-# from flask_app.models.user_model import User
+from flask_app.models.message_model import Message
 
 @app.route('/dashboard')
 def display_dashboard():
@@ -26,3 +26,13 @@ def process_post():
     }
     Post.save(data)
     return redirect('/dashboard')
+
+
+@app.route('/create/message', methods=['POST'])
+def process_message():
+    if 'email' not in session:
+        return redirect('/')
+
+    data = {
+        "content": request.form['content'],
+    }
