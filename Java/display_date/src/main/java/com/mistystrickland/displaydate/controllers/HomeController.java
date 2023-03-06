47d5bbef1089dad2de35ffec77394b88a7c26d15
Misty94 <mms.strickland@gmail.com>
2023-03-06 17:06:49 -0500

    practice

---
+++ Java/display_date/src/main/java/com/mistystrickland/displaydate/controllers/HomeController.java
@@ -0,0 +1,37 @@
+package com.mistystrickland.displaydate.controllers;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String home() {
+		return "home.jsp";
+	}
+	
+	@GetMapping("/date")
+	public String date(Model model) {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("EEEE, MMM d, yyyy");
+		Date date = new Date();
+		model.addAttribute("date", sdf.format(date));
+		
+		return "date.jsp";
+	}
+	
+	@GetMapping("/time")
+	public String time(Model model) {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("h:mm a");
+		Date date = new Date();
+		model.addAttribute("time", sdf.format(date));
+		
+		return "time.jsp";
+	}
+}
