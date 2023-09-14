f37f8d05bb08d413a9f427bb64610b9f797e25e5
Misty94 <mms.strickland@gmail.com>
2023-09-13 23:07:56 -0400

    algos

---
+++ Algos/arrays/in-progress/algo.js
@@ -0,0 +1,21 @@
+// num_list = [33,42,5,66,77,22,16,79,36,62,78,43,88,39,53,67,89,11]
+// for i in range(0, len(num_list)-1):
+//     for j in range(i+1, len(num_list)):
+//         if(num_list[i] > num_list[j]):
+//             num_list[i],num_list[j] = num_list[j], num_list[i]
+//     if i in range > 45:
+//             print(num_list)
+
+
+// function algo() {
+//     let num_list = [33,42,5,66,77,22,16,79,36,62,78,43,88,39,53,67,89,11];
+//     let i;
+//     let j;
+//     for (let i = 0; i < num_list.length; i++){
+//         for (let j = i+1; j < num_list){
+//             if (num_list[i] > num_list[j]){
+//                 num_list[i], num_list[j] = num_list[j], num_list[i]
+//             }
+//         }
+//     }
+// }
---
+++ Algos/arrays/in-progress/algo.py
@@ -0,0 +1,9 @@
+num_list = [33,42,5,66,77,22,16,79,36,62,78,43,88,39,53,67,89,11]
+for i in range(0, len(num_list)-1):
+    for j in range(i+1, len(num_list)):
+        if(num_list[i] > num_list[j]):
+            num_list[i],num_list[j] = num_list[j], num_list[i]
+    if i in range > 45:
+            print(num_list)
+    else: print("Hello")
+
