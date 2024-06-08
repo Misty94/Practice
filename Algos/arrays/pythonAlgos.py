23f0d8d83b41732b8bbc00cff99a87f3268a7525
Misty94 <mms.strickland@gmail.com>
2024-06-08 17:47:59 -0400

    python algos

--- Algos/arrays/pythonAlgos.py
+++ Algos/arrays/pythonAlgos.py
@@ -45,3 +45,37 @@ def firstPlusLength(list):
 
 print(firstPlusLength([1,2,3,4,5])) # 6
 print(firstPlusLength([2,4,6,8,10])) # 7
+
+
+# Write a function that accepts a list & creates a new list containing only the values from the original list that are greater than its 2nd value.
+# Print how many values this is & Return the new list
+# If the list has less than 2 elements, have the function return False
+
+def greaterValue(list):
+    if len(list) < 2:
+        return False
+    newList = []
+    count = 0
+    for i in range(len(list)):
+        if list[i] > list[1]:
+            newList.append(list[i])
+            count += 1
+    print(count) # Or I could have just print(len(newList))
+    return newList
+
+print(greaterValue([5,2,3,2,1,4]))
+print(greaterValue([3]))
+print(greaterValue([8,1,-3,5]))
+
+
+# Write a function that accepts two integers as parameters: size and value
+# The function should create and return a list whose length is equal to the given size & whose values are all the given value.
+
+def sizeValue(size, value):
+    list = []
+    for i in range(0, size):
+        list.append(value)
+    return list
+
+print(sizeValue(4,7)) # [7,7,7,7]
+print(sizeValue(6,2)) # [2,2,2,2,2,2]
