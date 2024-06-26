34f8198cd9500ec518867bec2f9723728db6cc45
Misty94 <mms.strickland@gmail.com>
2024-06-25 23:12:06 -0400

    flask notes

---
+++ Python/flask_notes.py
@@ -0,0 +1,12 @@
+# The HTTP request is made & hits the server.py file
+# Based on the route we give, it gathers up any HTML, CSS, JS, & data
+# Then it responds back to the browser with what we return
+
+# Virtual Environment = a way to keep organized & avoid conflicts when working on different projects with different versions and packages.
+
+# To Install WIndows: pip install pipenv
+# To Install Mac: pip3 install pipenv
+
+# You can create a folder, navigate into that folder using the terminal & use pipenv to create the environment & install Flask in one step
+# pipenv install flask
+
