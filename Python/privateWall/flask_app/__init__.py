516fdfa09a02a2b20b34ca48047780c78eb219a0
Misty94 <mms.strickland@gmail.com>
2024-07-27 17:53:33 -0400

    Private Wall

--- Python/privateWall/flask_app/__init__.py
+++ Python/privateWall/flask_app/__init__.py
@@ -4,5 +4,5 @@ import re
 app = Flask(__name__)
 app.secret_key = "nn7adjiaodifjeka&4##"
 
-DATABASE = "private_wall"
+DATABASE = "private_wall_schema"
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
