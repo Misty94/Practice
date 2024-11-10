cfb0dd75845cf531d39d94ae11420d2b8de48c19
Misty94 <mms.strickland@gmail.com>
2024-11-09 23:48:29 -0500

    Blurred Background Practice

--- WebFundamentals/blurredBackgroundPractice/blurredBackground.js
+++ WebFundamentals/blurredBackgroundPractice/blurredBackground.js
@@ -1,7 +1,11 @@
 
-function openForm() {
-    document.getElementById("popupForm").style.display = "block";
+function openPopup() {
+    document.getElementById('popupContainer').style.display='block';
+    // document.getElementById('overlay').style.display='none'; This made it regular - the popup appeared, but nothing was blurred.
+    document.getElementById('overlay').style.display='block'; // This made the background behind the popup blurred!!!
 }
-function closeForm() {
-    document.getElementById("popupForm").style.display = "none";
+
+function closePopup() {
+    document.getElementById('popupContainer').style.display='none';
+    document.getElementById('overlay').style.display='none';
 }
