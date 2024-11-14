482c5909d0ab9975579f73168f71f35f75315c10
Misty94 <mms.strickland@gmail.com>
2024-11-13 22:25:19 -0500

    double the integer algo

---
+++ Algos/integers/doubleInteger.js
@@ -0,0 +1,18 @@
+// Double the integer and return it.
+
+function doubleInteger(i) {
+    return i*2;
+}
+
+console.log(doubleInteger(2))  // 4
+console.log(doubleInteger(8))  // 16
+console.log(doubleInteger(5))  // 10
+console.log(doubleInteger(15))  // 30
+
+
+// ***********************************************
+
+
+const doubleInt = i => { return i * 2};
+
+console.log(doubleInt(100)) // 200
